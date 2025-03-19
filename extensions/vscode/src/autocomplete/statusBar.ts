116a730aaa9e22df3365b08d8e1340bb7562658a
duydq <duydq@rabiloo.com>
2025-03-19 13:47:18 +0700

    Update app name to ethery of Rabiloo

--- extensions/vscode/src/autocomplete/statusBar.ts
+++ extensions/vscode/src/autocomplete/statusBar.ts
@@ -47,20 +47,20 @@ const statusBarItemText = (
   error?: boolean,
 ) => {
   if (error) {
-    return "$(alert) Continue (FATAL ERROR)";
+    return "$(alert) Ethery (FATAL ERROR)";
   }
 
   switch (status) {
     case undefined:
       if (loading) {
-        return "$(loading~spin) Continue";
+        return "$(loading~spin) Ethery";
       }
     case StatusBarStatus.Disabled:
-      return "$(circle-slash) Continue";
+      return "$(circle-slash) Ethery";
     case StatusBarStatus.Enabled:
-      return "$(check) Continue";
+      return "$(check) Ethery";
     case StatusBarStatus.Paused:
-      return "$(debug-pause) Continue";
+      return "$(debug-pause) Ethery";
   }
 };
 
