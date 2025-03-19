1939cdb499abf0519a3027b762dbc8ef4abc8361
duydq <duydq@rabiloo.com>
2025-03-19 13:47:18 +0700

    Update app name to ethery of Rabiloo

--- extensions/vscode/e2e/selectors/GUI.selectors.ts
+++ extensions/vscode/e2e/selectors/GUI.selectors.ts
@@ -168,6 +168,6 @@ export class GUISelectors {
   }
 
   public static getContinueExtensionBadge(view: WebView) {
-    return SelectorUtils.getElementByAriaLabel(view, "Continue");
+    return SelectorUtils.getElementByAriaLabel(view, "Ethery");
   }
 }
