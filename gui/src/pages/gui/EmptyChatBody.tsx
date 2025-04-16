f2dae268ebfeded7b53c33060bcf1db4ac70b7de
LeDinhManh-RabilooTech <manhld@rabiloo.com>
2025-04-16 15:33:46 +0700

    update: hide ExploreHubCard

--- gui/src/pages/gui/EmptyChatBody.tsx
+++ gui/src/pages/gui/EmptyChatBody.tsx
@@ -27,7 +27,7 @@ export function EmptyChatBody({ showOnboardingCard }: EmptyChatBodyProps) {
 
   return (
     <div className="mx-2 mt-2">
-      <ExploreHubCard />
+      {/*<ExploreHubCard />*/}
       <ConversationStarterCards />
     </div>
   );
